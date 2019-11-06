d7f3bd06753d7d03067632236ec3234b6cb2df96
nikolapantelic-ftn <nikolapantelic434@gmail.com>
2019-11-06 21:53:26 +0100

    Napravio model studenta

--- src/ssluzba/app/Student.java
+++ src/ssluzba/app/Student.java
@@ -1,5 +1,122 @@
 package ssluzba.app;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.Date;
 
+enum Status { B, S }
+
+public class Student {
+	private String ime, prezime, adresaStanovanja, kontaktTelefon, email, brIndeksa;
+	private Date datumRodjenja, datumUpisa;
+	private double prosecnaOcena;
+	Status status;
+	private ArrayList<Predmet> predmeti;
+	
+	public Student() {}
+
+	public Student(String ime, String prezime, String adresaStanovanja, String kontaktTelefon, String email,
+			String brIndeksa, Date datumRodjenja, Status status) {
+		super();
+		this.ime = ime;
+		this.prezime = prezime;
+		this.adresaStanovanja = adresaStanovanja;
+		this.kontaktTelefon = kontaktTelefon;
+		this.email = email;
+		this.brIndeksa = brIndeksa;
+		this.datumRodjenja = datumRodjenja;
+		this.status = status;
+		this.datumUpisa = new Date();
+		this.predmeti = new ArrayList<Predmet>();
+		this.prosecnaOcena = 0;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+
+	public String getKontaktTelefon() {
+		return kontaktTelefon;
+	}
+
+	public void setKontaktTelefon(String kontaktTelefon) {
+		this.kontaktTelefon = kontaktTelefon;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getBrIndeksa() {
+		return brIndeksa;
+	}
+
+	public void setBrIndeksa(String brIndeksa) {
+		this.brIndeksa = brIndeksa;
+	}
+
+	public Date getDatumRodjenja() {
+		return datumRodjenja;
+	}
+
+	public void setDatumRodjenja(Date datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+
+	public Date getDatumUpisa() {
+		return datumUpisa;
+	}
+
+	public void setDatumUpisa(Date datumUpisa) {
+		this.datumUpisa = datumUpisa;
+	}
+
+	public double getProsecnaOcena() {
+		return prosecnaOcena;
+	}
+
+	public void setProsecnaOcena(double prosecnaOcena) {
+		this.prosecnaOcena = prosecnaOcena;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public ArrayList<Predmet> getPredmeti() {
+		return predmeti;
+	}
+
+	public void setPredmeti(ArrayList<Predmet> predmeti) {
+		this.predmeti = predmeti;
+	}
+	
+	
 }
