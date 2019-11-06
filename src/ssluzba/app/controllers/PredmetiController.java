bde1deeb8a7b9a1b4051c8f6e099bf10d924119c
nikolapantelic-ftn <nikolapantelic434@gmail.com>
2019-11-06 22:28:16 +0100

    Dodao kontrolere

---
+++ src/ssluzba/app/controllers/PredmetiController.java
@@ -0,0 +1,27 @@
+package ssluzba.app.controllers;
+
+public class PredmetiController implements Controller {
+
+	public PredmetiController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void dodaj() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisi() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izmeni() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ssluzba/app/controllers/ProfesoriController.java
@@ -0,0 +1,27 @@
+package ssluzba.app.controllers;
+
+public class ProfesoriController implements Controller {
+
+	public ProfesoriController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void dodaj() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisi() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izmeni() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
