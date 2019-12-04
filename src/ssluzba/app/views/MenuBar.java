51da2674c056348648555a096c08a6d7b8de2410
Nikola Pantelic <nikolapantelic434@gmail.com>
2019-12-05 00:40:09 +0100

    #menu_bar Napravljen meni bar (ispravka)

--- src/ssluzba/app/views/MenuBar.java
+++ src/ssluzba/app/views/MenuBar.java
@@ -1,49 +1,72 @@
 package ssluzba.app.views;
 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
-public class Toolbar extends JMenuBar implements ActionListener {
+public class MenuBar extends JMenuBar implements ActionListener {
 	
 	private static final long serialVersionUID = 5446478703932520621L;
 	
-	JButton dugmeDodaj;
-	JButton dugmeIzmeni;
-	JButton dugmeObrisi;
 
-	public Toolbar() {
-		dugmeDodaj = new JButton("Neka ikonica");
-		dugmeIzmeni = new JButton("ikonica");
-		dugmeObrisi = new JButton("Ikonica");
+	public MenuBar() {
+		initComponents();
+	}
+
+
+	private void initComponents() {
+		JMenu menuNew = new JMenu("New");
+		menuNew.setMnemonic(KeyEvent.VK_N);
+		JMenu menuEdit = new JMenu("Edit");
+		menuEdit.setMnemonic(KeyEvent.VK_E);
+		JMenu menuHelp = new JMenu("Help");
+		menuHelp.setMnemonic(KeyEvent.VK_H);
+		JMenuItem menuItemNew = new JMenuItem("New");
+		menuItemNew.setIcon(createImageIcon("new.png", ""));
+		menuItemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));	
+		JMenuItem menuItemClose = new JMenuItem("Close");
+		menuItemClose.setIcon(createImageIcon("close.png", ""));
+		menuItemClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
+		JMenuItem menuItemEdit = new JMenuItem("Edit");
+		menuItemEdit.setIcon(createImageIcon("edit.png", ""));
+		menuItemEdit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.ALT_MASK));
+		JMenuItem menuItemDelete = new JMenuItem("Delete");
+		menuItemDelete.setIcon(createImageIcon("delete.png", ""));
+		menuItemDelete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.ALT_MASK));
+		JMenuItem menuItemHelp = new JMenuItem("Help");
+		menuItemHelp.setIcon(createImageIcon("help.png", ""));
+		menuItemHelp.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.ALT_MASK));
+		JMenuItem menuItemAbout = new JMenuItem("About");
+		menuItemAbout.setIcon(createImageIcon("about.png", ""));
+		menuItemAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
+		menuNew.add(menuItemNew);
+		menuNew.add(menuItemClose);
+		menuEdit.add(menuItemEdit);
+		menuEdit.add(menuItemDelete);
+		menuHelp.add(menuItemHelp);
+		menuHelp.add(menuItemAbout);
 		
-		dugmeDodaj.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//uradi nesto
-			}
-		});
+		this.add(menuNew);
+		this.add(menuEdit);
+		this.add(menuHelp);
 		
-		dugmeIzmeni.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//uradi nesto
-			}
-		});
-		
-		dugmeObrisi.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//uradi nesto
-			}
-		});
-		
-		add(dugmeDodaj);
-		add(dugmeIzmeni);
-		add(dugmeObrisi);
-		
+	}
+	
+	public ImageIcon createImageIcon(String path, String description) {
+		java.net.URL imgURL = getClass().getResource("/ssluzba/resources/" + path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL, description);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
 	}
 
 
