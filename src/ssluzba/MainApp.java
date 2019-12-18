849ff169f164b26d918cb0ec21960ecf3668df33
Nikola Pantelic <nikolapantelic434@gmail.com>
2019-12-18 19:34:55 +0100

    MainFrame Singleton

--- src/ssluzba/MainApp.java
+++ src/ssluzba/MainApp.java
@@ -3,9 +3,8 @@ package ssluzba;
 import ssluzba.app.views.MainFrame;
 
 public class MainApp {
-
 	public static void main(String[] args) {
-		MainFrame mf = new MainFrame();
+		MainFrame mf = MainFrame.getInstance();
 
 	}
 
--- src/ssluzba/app/views/MainFrame.java
+++ src/ssluzba/app/views/MainFrame.java
@@ -2,8 +2,6 @@ package ssluzba.app.views;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
-import java.awt.HeadlessException;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
@@ -11,8 +9,17 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = -7911276033183096362L;
+	
+	private static MainFrame instance = null;
+	
+	public static MainFrame getInstance() {
+		if (instance == null) {
+			instance = new MainFrame();
+		}
+		return instance;
+	}
 
-	public MainFrame() throws HeadlessException {
+	private MainFrame() {
 		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize((int)(screenDimension.width*0.75), (int)(screenDimension.height*0.75));
 		setLocationRelativeTo(null);
@@ -21,21 +28,6 @@ public class MainFrame extends JFrame {
 		
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
-	public MainFrame(GraphicsConfiguration gc) {
-		super(gc);
-		// TODO Auto-generated constructor stub
-	}
-
-	public MainFrame(String title) throws HeadlessException {
-		super(title);
-		// TODO Auto-generated constructor stub
-	}
-
-	public MainFrame(String title, GraphicsConfiguration gc) {
-		super(title, gc);
-		// TODO Auto-generated constructor stub
 	}
 
 }
