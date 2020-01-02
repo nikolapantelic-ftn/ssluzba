f35e45acff937460af61d2e25e6fe0112bd35bd1
Nikola Pantelic <nikolapantelic434@gmail.com>
2020-01-02 15:26:53 +0100

    #dodavanje_studenta slanje sadrzaja polja

--- src/ssluzba/app/views/StudentDialog.java
+++ src/ssluzba/app/views/StudentDialog.java
@@ -16,71 +16,99 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
+import ssluzba.app.controllers.StudentiController;
+
 public class StudentDialog extends JDialog {
 
 	private static final long serialVersionUID = -1851059461360849827L;
-	
-	private boolean samofinansiranje, budzet;
-	
+	private JRadioButton samofinansiranjeButton, budzetButton;
+	private JTextField imeText, prezimeText, datumRodjenjaText, adresaStanovanjaText, brojTelefonaText, brojIndeksaText, emailText;
+
 	public StudentDialog() {
 		this.setTitle("Dodavanje Studenta");
 		this.setLayout(new FlowLayout());
 		this.setModal(true);
-		this.setSize(400,350);
+		this.setSize(400, 450);
 		this.setLocationRelativeTo(MainFrame.getInstance());
 		initComponents();
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
-	
+
 	private void initComponents() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-		panel.setPreferredSize(new Dimension(360, 300));
-		
+		panel.setPreferredSize(new Dimension(380, 400));
+
 		JPanel imePane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		imePane.add(new JLabel("Ime "));
-		JTextField imeText = new JTextField(20);
+		imeText = new JTextField(20);
 		imePane.add(imeText);
-		
+
 		JPanel prezimePane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		prezimePane.add(new JLabel("Prezime "));
-		JTextField prezimeText = new JTextField(20);
+		prezimeText = new JTextField(20);
 		prezimePane.add(prezimeText);
-		
+
 		JPanel datumRodjenjaPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		datumRodjenjaPane.add(new JLabel("Datum Rodjenja "));
-		JTextField datumRodjenjaText = new JTextField(20);
+		datumRodjenjaText = new JTextField(20);
 		datumRodjenjaPane.add(datumRodjenjaText);
 		
+		JPanel adresaStanovanjaPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+		adresaStanovanjaPane.add(new JLabel("Adresa Stanovanja"));
+		adresaStanovanjaText = new JTextField(20);
+		adresaStanovanjaPane.add(adresaStanovanjaText);	
+
 		JPanel brojTelefonaPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		brojTelefonaPane.add(new JLabel("Broj Telefona "));
-		JTextField brojTelefonaText = new JTextField(20);
+		brojTelefonaText = new JTextField(20);
 		brojTelefonaPane.add(brojTelefonaText);
 		
+		JPanel emailPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+		emailPane.add(new JLabel("E-mail "));
+		emailText = new JTextField(20);
+		emailPane.add(emailText);
+
 		JPanel brojIndeksaPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		brojIndeksaPane.add(new JLabel("Broj Indeksa "));
-		JTextField brojIndeksaText = new JTextField(20);
+		brojIndeksaText = new JTextField(20);
 		brojIndeksaPane.add(brojIndeksaText);
-		
+
 		JPanel godinaStudijaPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		godinaStudijaPane.add(new JLabel("Trenutna Godina Studija "));
-		String[] godinaStudijaStrings = {"I (prva)", "II (druga)", "III (treca)", "IV (cetvrta)"};
+		String[] godinaStudijaStrings = { "I (prva)", "II (druga)", "III (treca)", "IV (cetvrta)" };
 		JComboBox<String> godinaStudijaCombo = new JComboBox<String>(godinaStudijaStrings);
 		godinaStudijaPane.add(godinaStudijaCombo);
-		
+
 		ButtonGroup finansiranjeGroup = new ButtonGroup();
 		JPanel finansiranjePane = new JPanel();
 		finansiranjePane.setLayout(new BoxLayout(finansiranjePane, BoxLayout.PAGE_AXIS));
-		finansiranjePane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Nacin finansiranja"));
-		JRadioButton samofinansiranjeButton = new JRadioButton("Samofinansiranje", samofinansiranje);
-		JRadioButton budzetButton = new JRadioButton("Budzet", budzet);
+		finansiranjePane
+				.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Nacin finansiranja"));
+		samofinansiranjeButton = new JRadioButton("Samofinansiranje");
+		budzetButton = new JRadioButton("Budzet");
 		finansiranjeGroup.add(samofinansiranjeButton);
 		finansiranjeGroup.add(budzetButton);
 		finansiranjePane.add(samofinansiranjeButton);
 		finansiranjePane.add(budzetButton);
-		
+
 		JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		JButton potvrdaButton = new JButton("Potvrda");
+		potvrdaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String ime = StudentDialog.this.getImeText();
+				String prezime = StudentDialog.this.getPrezimeText();
+				String adresaStanovanja = StudentDialog.this.getAdresaStanovanjaText();
+				String kontaktTelefon = StudentDialog.this.getBrojTelefonaText();
+				String email = StudentDialog.this.getEmailText();
+				String brIndeksa = StudentDialog.this.getBrojIndeksaText();
+				String datumRodjenja = StudentDialog.this.getDatumRodjenjaText();
+				boolean samofinansiranje = StudentDialog.this.getSamofinansiranjeText();
+				boolean budzet = StudentDialog.this.getBudzetText();
+				StudentiController.getInstance().dodaj(ime, prezime, adresaStanovanja, kontaktTelefon, email, brIndeksa,
+						datumRodjenja, samofinansiranje, budzet);
+			}
+		});
 		JButton odustanakButton = new JButton("Odustanak");
 		odustanakButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -89,17 +117,55 @@ public class StudentDialog extends JDialog {
 		});
 		buttonPane.add(odustanakButton);
 		buttonPane.add(potvrdaButton);
-		
+
 		panel.add(imePane);
 		panel.add(prezimePane);
 		panel.add(datumRodjenjaPane);
 		panel.add(brojTelefonaPane);
+		panel.add(emailPane);
+		panel.add(adresaStanovanjaPane);
 		panel.add(brojIndeksaPane);
 		panel.add(godinaStudijaPane);
 		panel.add(finansiranjePane);
 		panel.add(buttonPane);
-		
+
 		this.add(panel);
+	}
+	
+	protected String getImeText() {
+		return imeText.getText();
+	}
+
+	protected String getPrezimeText() {
+		return prezimeText.getText();
+	}
+	
+	protected String getDatumRodjenjaText() {
+		return datumRodjenjaText.getText();
+	}
+	
+	protected String getAdresaStanovanjaText() {
+		return adresaStanovanjaText.getText();
+	}
+	
+	protected String getBrojTelefonaText() {
+		return brojTelefonaText.getText();
+	}
+	
+	protected String getEmailText() {
+		return emailText.getText();
+	}
+	
+	protected String getBrojIndeksaText() {
+		return brojIndeksaText.getText();
+	}
+	
+	protected boolean getSamofinansiranjeText() {
+		return samofinansiranjeButton.isSelected();
+	}
+	
+	protected boolean getBudzetText() {
+		return budzetButton.isSelected();
 	}
 
 }
